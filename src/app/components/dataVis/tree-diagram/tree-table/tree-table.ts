388c1394f1a5fd92448c17eff26d96a7771972b2
Chris <chris.m.clarke@live.co.uk>
2019-12-18 13:51:06 +0000

    WiP - add tree table export support

--- src/app/components/dataVis/tree-diagram/tree-table/tree-table.ts
+++ src/app/components/dataVis/tree-diagram/tree-table/tree-table.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { NgRedux, select } from "@angular-redux/store";
 import { Subject, Subscription, Observable } from "rxjs";
 import {
@@ -19,6 +19,7 @@ import { takeUntil } from "rxjs/operators";
   styleUrls: ["./tree-table.scss"]
 })
 export class TreeTableComponent {
+  @Input() exportMode: boolean;
   samplingStages: StageMeta[] = [];
   allocationSampleSize: number = -1;
   stageStrata: string[][];
--- src/app/pages/tool/summary/summary.module.ts
+++ src/app/pages/tool/summary/summary.module.ts
@@ -6,9 +6,16 @@ import { IonicModule } from "@ionic/angular";
 
 import { SummaryPage } from "./summary.page";
 import { PageComponentsModule } from "src/app/components/page/pageComponentsModule";
+import { DataVisComponentsModule } from "src/app/components/dataVis/dataVisComponents.module";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, PageComponentsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PageComponentsModule,
+    DataVisComponentsModule
+  ],
   declarations: [SummaryPage],
   entryComponents: [SummaryPage]
 })
